49f8d8cffb58052f54d9d5a76fea029ca57975da
heyitsjearbear <jereberries1215@gmail.com>
2023-12-29 01:29:31 -0800

    Added jwt authentication for protected routes.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 exports.signup = async (req, res) => {
   const { username, password } = req.body;
@@ -9,14 +10,20 @@ exports.signup = async (req, res) => {
   res.status(201).send("User registered successfully");
 };
 
-exports.signin = async (req, res) => {
+exports.signin = async (req, res, next) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
     if (!user) return res.status(400).send("User not found");
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).send("Invalid credentials");
-    res.status(200).send("User logged in successfully");
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).send({ token });
   } catch (err) {
     next(err);
   }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const authController = require('../controllers/authController');
-
+const express = require("express");
+const authController = require("../controllers/authController");
+const requireAuth = require("../middleware/auth");
 const router = express.Router();
 
-router.post('/signup', authController.signup);
-router.post('/signin', authController.signin);
+router.post("/signup", authController.signup);
+router.post("/signin", authController.signin);
+router.get("/protected", requireAuth, (req, res) => {
+  res.send("You are authenticated");
+});
 
 module.exports = router;
