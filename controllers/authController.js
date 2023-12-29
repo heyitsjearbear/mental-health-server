d7aac55603a628d1d5595d3aee13d0e379045b4d
heyitsjearbear <jereberries1215@gmail.com>
2023-12-29 01:10:38 -0800

    Update authController.js and index.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,23 @@
-const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
   const { username, password } = req.body;
   const user = new User({ username, password });
   await user.save();
-  res.status(201).send('User registered successfully');
+  res.status(201).send("User registered successfully");
 };
 
 exports.signin = async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  if (!user) return res.status(400).send('User not found');
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) return res.status(400).send('Invalid credentials');
-  res.status(200).send('User logged in successfully');
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).send("User not found");
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).send("Invalid credentials");
+    res.status(200).send("User logged in successfully");
+  } catch (err) {
+    next(err);
+  }
 };
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
 
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI);
 
 app.use(express.json());
-app.use('/api', authRoutes);
+app.use("/api", authRoutes);
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send({ error: err.message });
+});
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(3000, () => console.log("Server is running on port 3000"));
