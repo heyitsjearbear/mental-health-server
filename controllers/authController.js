22b559cb7162b386c52abaf9b18aa5fc1d1fe358
heyitsjearbear <jereberries1215@gmail.com>
2023-12-29 03:24:57 -0800

    Refactor authController and index.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,16 +7,17 @@ exports.signup = async (req, res) => {
   const { email, password } = req.body;
   const user = new User({ email, password });
   await user.save();
-  res.status(201).send("User registered successfully");
+  res.status(201).json({ message: "User registered successfully" });
 };
 
 exports.signin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).send("User not found");
+    if (!user) return res.status(400).json({ message: "User not found" });
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).send("Invalid credentials");
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
 
     // Generate JWT token
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 require("dotenv").config();
-
+const cors = require("cors");
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI);
 
+app.use(cors());
 app.use(express.json());
-app.use("/api", authRoutes);
-app.use((err, req, res, next) => {
-  res.status(err.status || 500).send({ error: err.message });
-});
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.use("/api/", authRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
