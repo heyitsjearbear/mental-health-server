aefac92127c484e2d70b88de75c41b21ac464503
heyitsjearbear <jereberries1215@gmail.com>
2023-12-29 17:18:38 -0800

    Updated userModel so that password doesn't get rehashed after user adds a journal

--- models/userModel.js
+++ models/userModel.js
@@ -9,9 +9,13 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function (next) {
-  const salt = await bcrypt.genSalt();
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+    if (!this.isModified("password")) {
+      return next();
+    }
+  
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
 
 module.exports = mongoose.model("User", userSchema);
--- routes/journalRoutes.js
+++ routes/journalRoutes.js
@@ -3,7 +3,7 @@ const journalController = require("../controllers/journalController");
 const router = express.Router();
 
 router.post("/:userId/journals", journalController.createJournal);
-router.get("/:userId/journals/", journalController.getJournals);
+router.get("/:userId/journals", journalController.getJournals);
 router.get("/:userId/journals/:journalId", journalController.getJournal);
 router.put("/:userId/journals/:journalId", journalController.updateJournal);
 router.delete("/:userId/journals/:journalId", journalController.deleteJournal);
